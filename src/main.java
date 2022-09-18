2e3dbf110789fd7a5cf1cf264f492b581ef640b3
Truonggiahuy <giahuytruong@H-Macbook.local>
2022-09-18 15:07:16 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+
+public class main {
+    public static void main(String[] args) {
+        int intOperaA = 1;
+        int intOperaB = 5;
+        int intSum ;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        double intOperaA = 1;
+        double intOperaB = 5;
+        double intSum ;
+        double intProduct;
+        double intDifference;
+        double intQuotient;
+
+        intSum = intOperaA + intOperaB ;
+        System.out.println("Sum is " +intSum);
+
+        
+
+    }
+
+
+}
